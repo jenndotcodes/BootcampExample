35eea4440bf9bd360df581435a5cf3990d921ce8
Jenn <reddunrogue@gmail.com>
2022-08-25 13:21:44 -0600

    Week 2 Example Created
    Created an Example for Week 2

--- Week 2/app/src/main/java/edu/aims/listofnames/MainActivity.kt
+++ Week 2/app/src/main/java/edu/aims/listofnames/MainActivity.kt
@@ -2,10 +2,46 @@ package edu.aims.listofnames
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.core.view.children
+import edu.aims.listofnames.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    private val names = arrayListOf(Person("Jenn","reading"),
+        Person("Jim","Watching Startrek"),
+        Person("Jill","Swimming"),
+        Person("Joe"),
+        Person("Jim","Watching Startrek"))
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+
+        // set content view after binding
+        val view = binding.root
+        setContentView(view)
+
+        val mainGroup = binding.mainGroup
+
+        var count = 0;
+        for(i in mainGroup.children)
+        {
+            if(i is TextView)
+            {
+                var content = names[count].name
+                if(names[count].favoriteHobby != null) {
+                    content += " " + names[count].favoriteHobby
+                }
+
+                i.text = content
+                count++;
+            }
+        }
+
+
     }
 }
---
+++ Week 2/app/src/main/java/edu/aims/listofnames/Person.kt
@@ -0,0 +1,3 @@
+package edu.aims.listofnames
+
+data class Person(val name: String, val favoriteHobby: String? = null)
